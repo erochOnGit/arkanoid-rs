fa8ddfb699666f391367bfd3aaa4b6a232c6504f
Christophe MASSOLIN <christophe.massolin@gmail.com>
2020-04-14 01:26:20 +0200

    Draw rect

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+import { WASM } from "./wasm.js"
+import { Renderer } from "./renderer.js"
+
+async function main() {
+
+  // Load WASM
+  const w = await WASM.load('./target/wasm32-unknown-unknown/debug/arkanoid-rust.wasm')
+
+  // Create canvas context
+  w.renderer = new Renderer()
+
+  w.exports.main()
+}
+
+window.addEventListener('DOMContentLoaded', main) 
